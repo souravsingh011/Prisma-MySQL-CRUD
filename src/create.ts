0b70762b7e70da067435e9630b00a849fce2fc9d
Adnan <addo@MacBook-Pro-2.local>
2022-02-13 20:53:01 +0100

    Prisma MySQL CRUD Init

---
+++ src/create.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const data = [
+      { name: 'Adnan Halilovic', description: 'Software development things', subscribers: 514, link: 'https://www.youtube.com/c/AdnanHalilovicDev' },
+      { name: 'Adnan Halilovic 2', description: 'Software development things', subscribers: 515, link: 'https://www.youtube.com/c/AdnanHalilovicDev' },
+      { name: 'Adnan Halilovic 3', description: 'Software development things', subscribers: 516, link: 'https://www.youtube.com/c/AdnanHalilovicDev' },
+      { name: 'Adnan Halilovic 4', description: 'Software development things', subscribers: 517, link: 'https://www.youtube.com/c/AdnanHalilovicDev' },
+    ];
+
+    const channel = { name: 'Adnan Halilovic 5', description: 'Software development things', subscribers: 601, link: 'https://www.youtube.com/c/AdnanHalilovicDev' };
+
+    const res = await prisma.youtube_channels.create({
+      data: channel,
+    });
+
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    async () => {
+      await prisma.$disconnect();
+    };
+  }
+}
+
+main();
